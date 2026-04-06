974f94fecf23f9bdaeb43b6e6287f377b14baabe
gc-ed <georgios.chatzipapas@eurodyn.com>
2026-04-06 13:42:55 +0300

    Update setup_db.sql

--- provisioning/roles/internal_database/files/setup_db.sql
+++ provisioning/roles/internal_database/files/setup_db.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS users (
 
 -- Flag 6 is hidden in this table
 INSERT INTO users (username, password_hash, notes) VALUES 
-('admin_backup', 'RkxBRzZre3NxbF9kdW1wX3N1Y2Nlc3N9', 'Legacy backup account. Use this b64 string as a hint for the ATM controller later.'),
+('admin_backup', 'RkxBRzZ7c3FsX2R1bXBfc3VjY2Vzc30=', 'Legacy backup account. Use this b64 string as a hint for the ATM controller later.'),
 ('windows', '$2b$05$6SmxCW4Qt/MA8vqmEWlRXuk8k4rJHW4FJIY9v.hWAzlF1ILYDHL0K', 'Internal App Server User'); 
 
 -- Note: The hash above is 'IronVault2026!' in bcrypt format for the player to crack.
