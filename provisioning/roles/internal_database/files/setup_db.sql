70a73bf461afde1291aab78ded84b49eceb8e0f5
gc-ed <georgios.chatzipapas@eurodyn.com>
2026-04-07 13:46:46 +0300

    Update setup_db.sql

--- provisioning/roles/internal_database/files/setup_db.sql
+++ provisioning/roles/internal_database/files/setup_db.sql
@@ -5,9 +5,13 @@ CREATE TABLE IF NOT EXISTS users (
     notes VARCHAR(255)
 );
 
+-- Insert the domain hint
+INSERT INTO infrastructure_config (service_name, service_value) VALUES 
+('internal_domain', 'ironvault.local'),
+('domain_controller', '10.10.20.50'),
+('active_directory_auth', 'ENABLED');
+
 -- Flag 6 is hidden in this table
 INSERT INTO users (username, password_hash, notes) VALUES 
 ('admin_backup', 'RkxBRzZ7c3FsX2R1bXBfc3VjY2Vzc30=', 'Legacy backup account. Use this b64 string as a hint for the ATM controller later.'),
 ('windows', '$2b$05$6SmxCW4Qt/MA8vqmEWlRXuk8k4rJHW4FJIY9v.hWAzlF1ILYDHL0K', 'Internal App Server User'); 
-
--- Note: The hash above is 'IronVault2026!' in bcrypt format for the player to crack.
